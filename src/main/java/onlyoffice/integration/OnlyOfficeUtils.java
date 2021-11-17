7ca999dc048f1aef888972743428a1db4e0c7de4
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2021-11-17 15:59:41 +0300

    support docxf

--- src/main/java/onlyoffice/integration/OnlyOfficeUtils.java
+++ src/main/java/onlyoffice/integration/OnlyOfficeUtils.java
@@ -74,7 +74,7 @@ public class OnlyOfficeUtils {
         return url;
     }
 
-    private List<String> editableExtensions = Arrays.asList("docx", "xlsx", "pptx");
+    private List<String> editableExtensions = Arrays.asList("docx", "xlsx", "pptx", "docxf");
     public boolean isEditable(String ext) {
         return editableExtensions.contains(trimDot(ext));
     }
@@ -182,7 +182,7 @@ public class OnlyOfficeUtils {
     }
 
     private String getDocType(String ext) {
-        if (".doc.docx.docm.dot.dotx.dotm.odt.fodt.ott.rtf.txt.html.htm.mht.pdf.djvu.fb2.epub.xps".indexOf(ext) != -1) return "text";
+        if (".doc.docx.docm.dot.dotx.dotm.odt.fodt.ott.rtf.txt.html.htm.mht.pdf.djvu.fb2.epub.xps.docxf".indexOf(ext) != -1) return "text";
         if (".xls.xlsx.xlsm.xlt.xltx.xltm.ods.fods.ots.csv".indexOf(ext) != -1) return "spreadsheet";
         if (".pps.ppsx.ppsm.ppt.pptx.pptm.pot.potx.potm.odp.fodp.otp".indexOf(ext) != -1) return "presentation";
         return null;
