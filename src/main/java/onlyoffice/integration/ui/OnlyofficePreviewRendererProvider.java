e68ed965fd2b68467665090c435ddf7708fee2e1
Sergey Linnik <sergey.linnik@onlyoffice.com>
2022-03-05 15:14:06 +0300

    support 7.3 Revert "fixed preview for portal-7.2"
    This reverts commit ed6bf942c779131096832998d001e6b1b6e68450.

--- src/main/java/onlyoffice/integration/ui/OnlyofficePreviewRendererProvider.java
+++ src/main/java/onlyoffice/integration/ui/OnlyofficePreviewRendererProvider.java
@@ -1,21 +1,3 @@
-/**
- *
- * (c) Copyright Ascensio System SIA 2022
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package onlyoffice.integration.ui;
 
 import com.liferay.document.library.preview.DLPreviewRenderer;
@@ -23,44 +5,73 @@ import com.liferay.document.library.preview.DLPreviewRendererProvider;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.util.WebKeys;
 
-import java.util.Optional;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 import onlyoffice.integration.config.OnlyOfficeConfigManager;
 
+@Component(
+        property = {
+            "service.ranking:Integer=100"
+        },
+        service = DLPreviewRendererProvider.class
+)
 public class OnlyofficePreviewRendererProvider implements DLPreviewRendererProvider {
 
-    public OnlyofficePreviewRendererProvider(ServletContext servletContext, OnlyOfficeConfigManager onlyOfficeConfigManager) {
-        _servletContext = servletContext;
-        _config = onlyOfficeConfigManager;
+    @Override
+    public Set<String> getMimeTypes() {
+        return new HashSet<>(Arrays.asList(
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+            "application/vnd.oasis.opendocument.text",
+            "application/vnd.oasis.opendocument.spreadsheet",
+            "application/vnd.oasis.opendocument.presentation",
+            "application/msword",
+            "application/vnd.ms-excel",
+            "application/vnd.ms-powerpoint",
+            "text/csv",
+            "text/rtf",
+            "text/plain",
+            "application/pdf"
+        ));
     }
 
     @Override
-    public Optional<DLPreviewRenderer> getPreviewDLPreviewRendererOptional(FileVersion fileVersion) {
+    public DLPreviewRenderer getPreviewDLPreviewRenderer(FileVersion fileVersion) {	
         if (!_config.webPreview()) {
-            return Optional.empty();
+            return null;
         }
 
-        return Optional.of( (request, response) -> {
-            RequestDispatcher requestDispatcher = _servletContext.getRequestDispatcher("/preview.jsp");
+        return (request, response) -> {
+            RequestDispatcher requestDispatcher = this.servletContext.getRequestDispatcher("/preview.jsp");
 
             request.setAttribute("fileId", fileVersion.getFileEntryId());
 
             requestDispatcher.include(request, response);
-        });
+        };
     }
 
     @Override
-    public Optional<DLPreviewRenderer> getThumbnailDLPreviewRendererOptional(FileVersion fileVersion) {
+    public DLPreviewRenderer getThumbnailDLPreviewRenderer(FileVersion fileVersion) {
         // TODO Auto-generated method stub
-        return Optional.empty();
+        return null;
     }
 
+    @Reference(target = "(osgi.web.symbolicname=onlyoffice.integration.web)")
+    protected ServletContext servletContext;
+
+    @Reference
     private OnlyOfficeConfigManager _config;
-    private final ServletContext _servletContext;
 
     private static final Log _log = LogFactoryUtil.getLog(OnlyofficePreviewRendererProvider.class);
 }
