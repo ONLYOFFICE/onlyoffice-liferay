5b83ede34203bf3be2ff55b4622d35c9b9789c2f
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2022-04-05 12:21:39 +0300

    fixed closing conversion dialog

--- src/main/java/onlyoffice/integration/ui/EditMenuContext.java
+++ src/main/java/onlyoffice/integration/ui/EditMenuContext.java
@@ -170,7 +170,7 @@ extends BaseDLViewFileVersionDisplayContext {
         StringBuilder sb = new StringBuilder();
 
         sb.append("Liferay.Util.openWindow({");
-        sb.append("dialog: {cache:false,width:500,height:200,modal:true,resizable: false},");
+        sb.append("dialog: {destroyOnHide:true,cache:false,width:500,height:200,modal:true,resizable: false},");
         sb.append("title: '" + lang + "',id: ");
         sb.append("'onlyofficeConvertPopup',uri:'");
         sb.append(getConvertUrl() + "'});");
