c4cebd11e4ea46f217d1de3368de8ad844b89b27
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2022-10-24 14:59:00 +0300

    removed unused import

--- src/main/java/onlyoffice/integration/api/OnlyOfficeDocumentApi.java
+++ src/main/java/onlyoffice/integration/api/OnlyOfficeDocumentApi.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Scanner;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
