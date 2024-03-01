bf25ad3c58ba5795057c5769d7605cd6150ee6a8
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-03-01 16:40:06 +0300

    fix: ConvertActionPortlet changed to portlet v3

--- src/main/java/onlyoffice/integration/ui/ConvertActionPortlet.java
+++ src/main/java/onlyoffice/integration/ui/ConvertActionPortlet.java
@@ -35,7 +35,8 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 			"javax.portlet.init-param.template-path=/",
 			"javax.portlet.init-param.view-template=/convert.jsp",
 			"javax.portlet.security-role-ref=power-user,user",
-			"javax.portlet.resource-bundle=content.Language"
+			"javax.portlet.resource-bundle=content.Language",
+			"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
